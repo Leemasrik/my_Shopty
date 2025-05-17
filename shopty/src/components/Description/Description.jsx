362c5f0e40d1d8d4ab9ac7540e9a7cdb9027c814
leema <leemasrikarna@gmail.com>
2025-05-17 22:05:22 +0530

    Add shopty project files

---
+++ shopty/src/components/Description/Description.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Description.css';
+const Description = () => {
+  return (
+    <div className='description'>
+        <div className="description-navigator">
+            <div className="description-nav-box">
+                Description
+            </div>
+         <div className="description-nav-box-fade">
+            Reviews (122)</div>   
+        </div>
+        <div className="description-description">
+            <p>An e-commerce website is an online platform that allows users to browse, search, and purchase products or services over the internet. It functions as a digital storefront where businesses can showcase their offerings, and customers can shop conveniently from anywhere.</p>
+<p>The goal of this project is to simulate a real e-commerce shopping experience by implementing a frontend application where users can explore and view products by category and access product details.</p>
+        </div>
+    </div>
+  )
+}
+export default Description; 
